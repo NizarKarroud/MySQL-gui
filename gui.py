dbc2bf3e67c03de8da2c5b3de8a6d6f2c7e45638
Baragsen <nkarroud@gmail.com>
2024-02-13 20:21:09 +0000

    added the port entry in the login page , fixed the login logic

--- gui.py
+++ gui.py
@@ -13,6 +13,9 @@ label.pack(padx=20 , pady=22)
 host_entry = customtkinter.CTkEntry(frame, placeholder_text="hostname" , height=35)
 host_entry.pack(padx=10,pady=22)
 
+port_entry = customtkinter.CTkEntry(frame, placeholder_text="port(3306 by default)" , height=35)
+port_entry.pack(padx=10,pady=22)
+
 
 username_entry= customtkinter.CTkEntry(frame, placeholder_text="Username" , height=35)
 username_entry.pack(padx=10,pady=22)
@@ -20,7 +23,8 @@ username_entry.pack(padx=10,pady=22)
 password_entry= customtkinter.CTkEntry(frame, placeholder_text="Password" , show="*" , height=35)
 password_entry.pack(padx=10,pady=22)
 
-login_button= customtkinter.CTkButton(master=frame , text="Login" , command=mysql_con.handle_login(hostname= host_entry.get(),username= username_entry.get(),passw= password_entry.get()))
+login_button= customtkinter.CTkButton(master=frame , text="Login" , command= lambda: 
+mysql_con.handle_login(hostname= host_entry.get(),username= username_entry.get(),passw= password_entry.get() , port=port_entry.get()))
 login_button.pack(padx=10,pady=22 )
 
 
--- mysql_con.py
+++ mysql_con.py
@@ -1,16 +1,17 @@
 import mysql.connector
 global_connection = None
 
-def handle_login(hostname,username , passw):
+def handle_login(hostname,username , passw , port=3306):
     try : 
-        connection = mysql.connector.connect(host=hostname , user=username , password=passw)
+        connection = mysql.connector.connect(host=hostname , user=username , password=passw , port=port)
         if isinstance(connection , mysql.connector.connection.MySQLConnection) :
             globals()["global_connection"]= connection
             return "succesful"
+
         else :
-            ...
-            # implement the error handling
+            return ""
     except mysql.connector.Error as err:
+        print(err)
         return ""
 
 
