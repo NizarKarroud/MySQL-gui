c8cc40007a7b20bcc725782692a2415bbcea0221
Baragsen <nkarroud@gmail.com>
2024-03-25 19:48:26 +0000

    jusr comments indicating the options i can add for the future implementation

--- dump.py
+++ dump.py
@@ -1,12 +1,12 @@
 import subprocess
 
-def execute_mysql_dump(path ,hostname, user , password , database):
+def execute_mysql_dump(path,file_name ,hostname, user , password , database):
     try:
         # Open the output file in write mode
-        with open(fr"{path}", 'w') as output_file:
+        with open(fr"{path}\{file_name}.sql", 'w') as output_file:
             # Open MySQL shell using subprocess
             mysql_process = subprocess.Popen(
-                ['mysqldump', '-h', f'{hostname}', '-u', f'{user}', f'-p{password}', f"{database}"],
+                ['mysqldump', '-h', f'{hostname}', '-u', f'{user}', f'-p{password}', '--add-drop-table' , f'{database}'],
                 stdout=output_file,  # Redirect stdout to the output file
                 stderr=subprocess.PIPE,  # Capture stderr for error handling
                 universal_newlines=True
@@ -23,5 +23,16 @@ def execute_mysql_dump(path ,hostname, user , password , database):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+"""
+de base struct and data
+--no-data : just structure
+--no-create-info :  jusr data
+--add-drop-table : drop table if exist
+ --routines 
+ --events 
+"""
+
+
+
 # Call the function to execute mysqldump
-execute_mysql_dump()
+execute_mysql_dump(r"C:\Users\Nizar\Documents\mysql_gui" ,"etu_priv" , "localhost" , "root" , "root" , "etudiant")
